ae4895cb39a33772527334fe511407eaea3367d7
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2020-12-01 19:34:06 +0000

    day one part 2

--- day1/find_numbers_2020.rb
+++ day1/find_numbers_2020.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'set'
+require 'pry'
 
 # class to find the two numbers in the data tha when added together = 2020 and what the value of them multiplied is
 class FindNumbers2020
@@ -20,12 +21,43 @@ class FindNumbers2020
       corresponding_number = TOTAL - number
       next unless set.include?(corresponding_number)
 
+      puts 'Pairs'
       puts "numbers are #{number} and #{corresponding_number}"
       puts '-------'
+      puts 'multiplied = '
       puts corresponding_number * number
       break # as we only need the first pair
+    end
+  end
+
+  # we need to find 3 numbers in the list that add up to 2020 and then multiply them.
+  # set a flag so we can exit when we have found the 3
+  # go over the set and find the new remainder then go over the set again excluding the number we have in the main loop
+  # take the nuimber away from the running remainder and see if that is left in the set
+  def find_triplets_and_multiples
+    done = false
+    set.each do |number|
+      new_total = TOTAL - number
+      exit if done
+      set.each do |number1|
+        next if number1 == number
+
+        corresponding_number = new_total - number1
+        next unless set.include?(corresponding_number)
+
+        puts '-------------********-------------'
+        puts 'Triplets'
+        puts "numbers are #{number1}, #{number} and #{corresponding_number}"
+        puts '-------'
+        puts 'multiplied = '
+        puts corresponding_number * number * number1
+        done = true
+        break # as we only need the first triplets
+      end
     end
   end
 end
 
 FindNumbers2020.new.find_pairs_and_multiples
+
+FindNumbers2020.new.find_triplets_and_multiples
